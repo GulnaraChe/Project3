6dbecdba1ee1195cd40e4257c27958ebc1f2862c
Gulnara Che <g.chepurnova@gmail.com>
2023-03-24 10:22:28 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int bonus = 0;
+        int amount = 1200;
+        boolean bonus1 = amount > 1000;
+        if (bonus1) {
+            bonus = amount / 100;
+        } else {
+            bonus = 0;
+        }
+        System.out.println(bonus);
+    }
+}
